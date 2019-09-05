a82e60ff4456ac0065be96f02a014cf42daa60c3
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-09-05 19:26:00 +0530

    Duhh

--- Projects/FibonacciSeries.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------------------------- #
-#
-# !Python       :   3.7.3
-# Author        :   Nitin K (https://github.com/nkpydev)
-# Github        :   https://github.com/nkpydev
-# Description   :   This is pratical implementation of Luhn's Algorithm,
-#                   which is used to validate CreditCards, SSN nos, IMEI nos
-#
-# ---------------------------------------------------------------------------- #

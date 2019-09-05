64f711ff3f0ac0aba914999a552a4aa4491145a0
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-09-05 19:09:46 +0530

    Updated Program Name

--- Projects/CreditCardValidator.py
+++ Projects/CreditCardValidator.py
@@ -3,6 +3,7 @@
 # !Python       :   3.7.3
 # Author        :   Nitin K (https://github.com/nkpydev)
 # Github        :   https://github.com/nkpydev
+# Program       :   CreditCardValidator
 # Description   :   This is pratical implementation of Luhn's Algorithm,
 #                   which is used to validate CreditCards, SSN nos, IMEI nos
 #
